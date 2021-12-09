1358bcc42a0587d7c756a5c8b9127b94380070f2
João Hudson <joao.oliveira@projecti.com.br>
2021-12-09 19:22:12 -0300

    movendo porta para variaveis de ambiente

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
+require('dotenv/config');
 const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(path.join(__dirname, 'client')));
-app.listen(8080);
+app.listen(process.env.PORT);
